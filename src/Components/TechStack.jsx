101c34fc423b5124a7312446b67842dbc6f7820f
jon <ldegjon@gmail.com>
2023-08-17 12:22:53 +0200

    added another logo to the techstack

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const TechStack = () => {
-  const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,1,2,3,4,5,6];
+  const array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,1,2,3,4,5,6];
   return (
     <section className='' id='tech-stack'>
       <h2>Tech <span className='accent-text'>Stack</span></h2>
